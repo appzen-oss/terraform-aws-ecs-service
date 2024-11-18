fcc7da302b7d94133faeb2c194e4bef697df0383
MadhuriAndhale <msandhale4@gmail.com>
2024-11-18 14:33:03 +0530

    Firelens logs to opensearch via s3

--- main.tf
+++ main.tf
@@ -330,7 +330,7 @@ data "template_file" "firelens_container_definition" {
 # Look into support for sidecars, proxy, (AppMesh)
 
 locals {
-   container_definitions = "${var.container_definition == "" && var.firelens_host_url == "" ? element(concat(data.template_file.container_definition.*.rendered, list("")), 0) : "[${data.template_file.firelens_container_definition.rendered},${data.template_file.sidecar_container_definition.rendered},${data.template_file.telegraf_sidecar_container_definition.rendered},${data.template_file.promtail_sidecar_container_definition.rendered},${data.template_file.cleanup_sidecar_container_definition.rendered}]"}"
+   container_definitions = "${var.container_definition == "" && var.firelens_host_url == "" ? element(concat(data.template_file.container_definition.*.rendered, list("")), 0) : "[${data.template_file.firelens_container_definition.rendered},${data.template_file.sidecar_container_definition.rendered},${data.template_file.promtail_sidecar_container_definition.rendered}]"}"
 }
 
 resource "aws_ecs_task_definition" "task" {
